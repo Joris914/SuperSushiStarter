f4ad67f4383d78b8f7ad8309e1ab1bd7d156b324
Joris Koevoets <63403671+Joris914@users.noreply.github.com>
2020-10-05 03:13:45 +0200

    Start

---
+++ SusperSushiStarter.Web/Controllers/GerechtenController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SuperSushi.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperSushiStarter.Web.Controllers
+{
+	public class GerechtenController : Controller
+	{
+		IGerechtRepository repo = new GerechtRepositorySql();
+	}
+}
